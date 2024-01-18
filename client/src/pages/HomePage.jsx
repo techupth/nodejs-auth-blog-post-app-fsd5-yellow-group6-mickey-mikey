cec2ad78225b3f9749892cc9445260ed770e1ab8
Havocrazer <92991759+Havocrazer@users.noreply.github.com>
2024-01-18 19:24:36 +0700

    feat: add finish assignment

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/authentication";
 
 import usePosts from "../hooks/usePosts";
 import getPublishedDate from "../utils/getPublishedDate";
@@ -11,6 +12,8 @@ function HomePage() {
   const [keywords, setKeywords] = useState("");
   const { posts, totalPages, getPosts, deletePost, isError, isLoading } =
     usePosts();
+
+  const { logout } = useAuth();
 
   useEffect(() => {
     getPosts({ status, keywords, page });
@@ -27,11 +30,13 @@ function HomePage() {
         >
           Create Post
         </button>
-        {/* 
-           // 🐨 Todo: Exercise #7
-          //  นำ Function `logout` จาก AuthContext มา Execute ใน Prop `onClick`
-        */}
-        <button>Logout</button>
+        <button
+          onClick={() => {
+            logout();
+          }}
+        >
+          Logout
+        </button>
       </div>
       <div className="search-box-container">
         <div className="search-box">
--- server/app.js
+++ server/app.js
@@ -3,16 +3,25 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import postRouter from "./apps/posts.js";
 import { client } from "./utils/db.js";
+import authRouter from "./apps/auth.js";
+import dotenv from "dotenv";
 
 async function init() {
+  dotenv.config();
   const app = express();
   const port = 4000;
 
-  await client.connect();
+  try {
+    await client.connect();
+  } catch {
+    console.log("Cannot connect mongodb");
+  }
 
   app.use(cors());
   app.use(bodyParser.json());
+
   app.use("/posts", postRouter);
+  app.use("/auth", authRouter);
 
   app.get("/", (req, res) => {
     res.send("Hello World!");
